d765e8439d9189e43577a2b7faee71c2b82ccf06
tgitlawrie <tlawrie@orcon.net.nz>
2022-11-16 19:13:42 +1300

    before removing recycler

--- app/src/main/java/com/example/bit603_a3_tonylawrie/AppActivity.java
+++ app/src/main/java/com/example/bit603_a3_tonylawrie/AppActivity.java
@@ -18,8 +18,9 @@ import java.util.List;
 public class AppActivity extends AppCompatActivity {
 
   //  Fragment fragment;
-  FragmentManager fragmentManager;
-  FragmentTransaction fragmentTransaction;
+  static FragmentManager fragmentManager;
+  static FragmentTransaction fragmentTransaction;
+  BottomNavigationView bottomNavigationView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +32,9 @@ public class AppActivity extends AppCompatActivity {
     //load inventory fragment
     addFragment(new InventoryFragment(), InventoryFragment.fragmentID);
 
-    BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+    bottomNavigationView = findViewById(R.id.bottomNavigation);
     bottomNavigationView.setSelectedItemId(R.id.inventory); // sets active fragment icon
+    fragmentManager = getSupportFragmentManager();
 
     //item selected listener
     bottomNavigationView.setOnItemSelectedListener(item -> {
@@ -41,33 +43,13 @@ public class AppActivity extends AppCompatActivity {
           startActivity(new Intent(getApplicationContext(), MainActivity.class));
           overridePendingTransition(0, 0);
           break;
-        case R.id.add_item:
-          replaceFragment(new AddItemFragment(), AddItemFragment.fragmentID);
-          break;
+
         case R.id.inventory:
           replaceFragment(new InventoryFragment(), InventoryFragment.fragmentID);
           break;
-        case R.id.add_test_data:
-          addSampleData();
-          redrawFragment(InventoryFragment.fragmentID);
-          break;
-          case R.id.delete_items:
-            //WARNING! Resets inventory database, for development testing only
-            MainActivity.inventoryDb.inventoryDao().resetInventory(); //TODO Confirm
-            replaceFragment(new InventoryFragment(), InventoryFragment.fragmentID);
-            break;
       }
       return true;
     });
-  }
-
-  private void redrawFragment(String ID) {
-    fragmentManager = getSupportFragmentManager();
-    Fragment currentFragment = fragmentManager.findFragmentByTag(ID);
-    fragmentTransaction = fragmentManager.beginTransaction();
-    fragmentTransaction.detach(currentFragment);
-    fragmentTransaction.attach(currentFragment);
-    fragmentTransaction.commit();
   }
 
   // method to add initial fragment
@@ -79,36 +61,11 @@ public class AppActivity extends AppCompatActivity {
   }
 
   // replace fragment
-  public void replaceFragment(Fragment fragment, String ID) {
-    fragmentManager = getSupportFragmentManager();
+  public static void replaceFragment(Fragment fragment, String ID) {
     fragmentTransaction = fragmentManager.beginTransaction();
     fragmentTransaction.addToBackStack(null);
     fragmentTransaction.replace(R.id.app_container, fragment, ID);
     fragmentTransaction.commit();
-  }
-
-  public void addSampleData() {
-    AlertDialog.Builder builder = new AlertDialog.Builder(this);
-    builder.setTitle("Confirm")
-            .setMessage("This will add 20 sample items with random types and quantities to the Inventory, Are you sure you want to proceed?");
-    //positive confirmation
-    builder.setPositiveButton("Yes",(dialogInterface, i) -> {
-      // get sample list, iterate through and insert to db
-      List<Inventory> sample = Inventory.getSampleData();
-      for(Inventory item: sample){
-        MainActivity.inventoryDb.inventoryDao().addItem(item);
-        Toast toast = Toast.makeText(getBaseContext(), "Sample Data added successfully!", Toast.LENGTH_SHORT);
-        toast.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 40);
-        toast.show();
-
-        // Reload Inventory Fragment
-        replaceFragment(new InventoryFragment(), InventoryFragment.fragmentID);
-      }
-    });
-    // negative confirmation
-    builder.setNegativeButton("Cancel",(dialogInterface, i) -> {}); // do nothing
-    AlertDialog confirmation = builder.create();
-    confirmation.show();
   }
 
 }
--- app/src/main/java/com/example/bit603_a3_tonylawrie/InventoryFragment.java
+++ app/src/main/java/com/example/bit603_a3_tonylawrie/InventoryFragment.java
@@ -1,20 +1,27 @@
 package com.example.bit603_a3_tonylawrie;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.tabs.TabLayout;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -22,18 +29,19 @@ public class InventoryFragment extends Fragment {
   private static String TAG = "InventoryFragment";
   public static final String fragmentID = "inventory";
 
+  BottomNavigationView inventoryNavigationView;
 
+  private int pageSize = 5;
+  private int page = 1;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-
   }
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
-
     // Inflate the layout for this fragment
     return inflater.inflate(R.layout.fragment_inventory, container, false);
   }
@@ -41,20 +49,103 @@ public class InventoryFragment extends Fragment {
   @Override
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
+    inventoryNavigationView = view.findViewById(R.id.inventoryNavigation);
+
     List<Inventory> inventory = MainActivity.inventoryDb.inventoryDao().getInventory();
+    List<Inventory> pagedList = new ArrayList<>();// list to store paged results
+
+
+    //item selected listener
+    inventoryNavigationView.setOnItemSelectedListener(item -> {
+      switch (item.getItemId()) {
+        case R.id.next:
+          pagedList.removeAll(inventory);
+          nextPage();
+          break;
+        case R.id.add_item:
+          break;
+        case R.id.add_test_data:
+          addSampleData();
+          break;
+        case R.id.delete_items:
+          //WARNING! Resets inventory database, for development testing only
+          deleteData();
+          break;
+        case R.id.previous:
+          pagedList.removeAll(inventory);
+          prevPage();
+          break;
+      }
+      return true;
+    });
+
+    // only call paginate if inventory has items
+    if (inventory.size() > 0) {
+      for (int i = page * pageSize -pageSize; i < pageSize * page; i++) {
+        pagedList.add(inventory.get(i));
+      }
+    }
 
     RecyclerView recycler = view.findViewById(R.id.itemRecycler);
     recycler.setLayoutManager(new LinearLayoutManager(getContext()));
     recycler.setHasFixedSize(true);
-    ItemAdapter itemAdapter = new ItemAdapter(getContext(), inventory);
+    ItemAdapter itemAdapter = new ItemAdapter(getContext(), pagedList);
     recycler.setAdapter(itemAdapter);
     itemAdapter.notifyDataSetChanged();
+  }
+
+  private void deleteData() {
+    AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+    builder.setTitle("Delete all Items")
+            .setMessage("Are you Sure you want to delete all Items in the inventory?");
+    //positive confirmation
+    builder.setPositiveButton("Yes", (dialogInterface, i) -> {
+      // delete all data from the inventory
+      MainActivity.inventoryDb.inventoryDao().resetInventory();
+      Toast toast = Toast.makeText(getContext(), "Inventory deleted!", Toast.LENGTH_SHORT);
+      toast.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 40);
+      toast.show();
+      // replace fragment
+      AppActivity.replaceFragment(new InventoryFragment(), fragmentID);
+    });
+    // negative confirmation
+    builder.setNegativeButton("Cancel", null);
+
+    AlertDialog confirmation = builder.create();
+    confirmation.show();
+  }
 
 
-    for (Inventory item: inventory) {
-      String name = item.getItem();
-      String type = item.getType();
-      String quantity = String.valueOf(item.getQuantity());
-    }
+  public void addSampleData() {
+    AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+    builder.setTitle("Confirm")
+            .setMessage("This will add 20 sample items with random types and quantities to the Inventory, Are you sure you want to proceed?");
+    //positive confirmation
+    builder.setPositiveButton("Yes", (dialogInterface, i) -> {
+      // get sample list, iterate through and insert to db
+      List<Inventory> sample = Inventory.getSampleData();
+      for (Inventory item : sample) {
+        MainActivity.inventoryDb.inventoryDao().addItem(item);
+      }
+      Toast toast = Toast.makeText(getContext(), "Sample Data added successfully!", Toast.LENGTH_SHORT);
+      toast.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 40);
+      toast.show();
+      // replace fragment
+      AppActivity.replaceFragment(new InventoryFragment(), fragmentID);
+    });
+    // negative confirmation
+    builder.setNegativeButton("Cancel", null);
+
+    AlertDialog confirmation = builder.create();
+    confirmation.show();
+  }
+
+  private void nextPage() {
+    page++;
+  }
+
+  private void prevPage() {
+    page--;
+
   }
 }
